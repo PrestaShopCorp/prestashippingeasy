79b12a237bfa3d8e3a32d9477be0a62900c73a9c
cedricfontaine <cfontaine@spidmail.net>
2014-12-23 11:06:17 -0500

    Add check to json_data received

--- controllers/front/callback.php
+++ controllers/front/callback.php
@@ -31,36 +31,41 @@ class prestashippingeasycallbackModuleFrontController extends ModuleFrontControl
 		$values = Tools::file_get_contents('php://input');
 		$output = Tools::jsonDecode($values, true);
 
-      	$order_id = $output['shipment']['orders'][0]['external_order_identifier'];
-        $tracking_number = $output['shipment']['tracking_number'];
-        $carrier_key = $output['shipment']['carrier_key'];
-        $carrier_service_key = $output['shipment']['carrier_service_key'];
-        $shipment_cost_cents = $output['shipment']['shipment_cost'];
-        $shipment_cost = ($shipment_cost_cents / 100);
+      	if ($output)
+      	{
+	      	$order_id = $output['shipment']['orders'][0]['external_order_identifier'];
+	        $tracking_number = $output['shipment']['tracking_number'];
+	        $carrier_key = $output['shipment']['carrier_key'];
+	        $carrier_service_key = $output['shipment']['carrier_service_key'];
+	        $shipment_cost_cents = $output['shipment']['shipment_cost'];
+	        $shipment_cost = ($shipment_cost_cents / 100);
 
-        $order=new Order((int)$order_id);
-        if (Validate::isLoadedObject($order)) {
-        	$order->setCurrentState(Configuration::get('PS_OS_SHIPPING'));
-	        $comment_update = 'Shipping Tracking Number: ' . $tracking_number . ' Carrier Key: ' . $carrier_key . ' Carrier Service Key: ' . $carrier_service_key . ' Cost: ' . $shipment_cost;
+	        if ($order_id && $tracking_number)
+	        {
+		        $order=new Order((int)$order_id);
+		        if (Validate::isLoadedObject($order)) {
+		        	$order->setCurrentState(Configuration::get('PS_OS_SHIPPING'));
+			        $comment_update = 'Shipping Tracking Number: ' . $tracking_number . ' Carrier Key: ' . $carrier_key . ' Carrier Service Key: ' . $carrier_service_key . ' Cost: ' . $shipment_cost;
 
-			$msg = new Message();
-			$msg->message = Tools::substr($comment_update,0,1600);
-			$msg->id_order = (int)($order_id);
-			$msg->private = 1;
-			$msg->add();
+					$msg = new Message();
+					$msg->message = Tools::substr($comment_update,0,1600);
+					$msg->id_order = (int)($order_id);
+					$msg->private = 1;
+					$msg->add();
 
-        	if (Validate::isTrackingNumber($tracking_number)) {
-				$order->shipping_number = $tracking_number;
-				$order->update();
+		        	if (Validate::isTrackingNumber($tracking_number)) {
+						$order->shipping_number = $tracking_number;
+						$order->update();
 
-				$order_carrier = new OrderCarrier($order->getIdOrderCarrier());
-				if (Validate::isLoadedObject($order_carrier)) {
-					$order_carrier->tracking_number = $tracking_number;
-					$order_carrier->update();
+						$order_carrier = new OrderCarrier($order->getIdOrderCarrier());
+						if (Validate::isLoadedObject($order_carrier)) {
+							$order_carrier->tracking_number = $tracking_number;
+							$order_carrier->update();
+						}
+					}
 				}
 			}
 		}
-
- 	die();
+ 		die();
 	}
 }
